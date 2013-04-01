548a39431d481626eb84db01600c276a99676b26
esefah <esefah@mit.edu>
2013-04-01 00:24:30 -0400

    Added text view to location so we can see changes

--- LocationApplication/gen/com/example/locationapplication/R.java
+++ LocationApplication/gen/com/example/locationapplication/R.java
@@ -24,7 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int accuView=0x7f080005;
+        public static final int action_settings=0x7f080006;
+        public static final int latView=0x7f080003;
+        public static final int longView=0x7f080004;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080001;
+        public static final int textView3=0x7f080002;
     }
     public static final class layout {
         public static final int activity_location=0x7f030000;
@@ -35,6 +41,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int default0=0x7f050003;
         public static final int hello_world=0x7f050002;
     }
     public static final class style {
