ab56094c1e0d7dcaba482eaf3b291d4a17f75508
esefah <esefah@mit.edu>
2013-04-29 17:22:26 -0400

    add markers to parse

--- TabActionBar/src/com/example/tabactionbar/MapFragFake.java
+++ TabActionBar/src/com/example/tabactionbar/MapFragFake.java
@@ -2,6 +2,7 @@ package com.example.tabactionbar;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import android.annotation.TargetApi;
 import android.graphics.Color;
@@ -22,6 +23,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class MapFragFake extends Fragment {
@@ -144,6 +146,7 @@ public class MapFragFake extends Fragment {
         	// Check if we were successful in obtaining the map.
         	if (mMap != null) {
         		makeMarkers();
+        		loadMarkersFromParse();
         	}
         }
 
@@ -161,6 +164,14 @@ public class MapFragFake extends Fragment {
 		pseudoPos.add(longi);
 		scheduleMarkersOnMap.put(name,pseudoPos);
 		scheduleMarkerNames.add(name);
+		//add marker to parse
+		ParseObject storage=new ParseObject("MapMarkers");
+		storage.put("username", ParseUser.getCurrentUser().getUsername());
+		storage.put("type", "Schedule");
+		storage.put("name", name);
+		storage.put("longitude", longi);
+		storage.put("latitude",	lat);
+		storage.saveInBackground();
 		
 		// logging
 		ParseObject log = new ParseObject("MapFragment");
@@ -189,6 +200,14 @@ public class MapFragFake extends Fragment {
 		pseudoPos.add(longi);
 		todoMarkersOnMap.put(name,pseudoPos);
 		todoMarkerNames.add(name);
+		//add todo markers to parse
+		ParseObject storage=new ParseObject("MapMarkers");
+		storage.put("username", ParseUser.getCurrentUser().getUsername());
+		storage.put("type", "To Do");
+		storage.put("name", name);
+		storage.put("longitude", longi);
+		storage.put("latitude",	lat);
+		storage.saveInBackground();
 		
 		// logging
 		ParseObject log = new ParseObject("MapFragment");
@@ -217,7 +236,14 @@ public class MapFragFake extends Fragment {
 		pseudoPos.add(longi);
 		meetMarkersOnMap.put(name,pseudoPos);
 		meetMarkerNames.add(name);
-		
+		//add meet markers
+		ParseObject storage=new ParseObject("MapMarkers");
+		storage.put("username", ParseUser.getCurrentUser().getUsername());
+		storage.put("type", "Meet");
+		storage.put("name", name);
+		storage.put("longitude", longi);
+		storage.put("latitude",	lat);
+		storage.saveInBackground();
 		// logging
 		ParseObject log = new ParseObject("MapFragment");
 		log.put("action", "add marker meet");
@@ -335,5 +361,35 @@ public class MapFragFake extends Fragment {
         if (f != null) 
             getFragmentManager().beginTransaction().remove(f).commit();
     }
+	
+	private void loadMarkersFromParse(){
+		com.parse.ParseQuery query = new com.parse.ParseQuery("Map Markers");
+		query.whereEqualTo("username", ParseUser.getCurrentUser().getUsername());
+		query.findInBackground(new com.parse.FindCallback() {
+			@Override
+			public void done(List<ParseObject> arg0, com.parse.ParseException arg1) {
+				// TODO Auto-generated method stub
+				if (arg1 == null) {
+					if (!arg0.isEmpty()) {
+						for (int i = 0; i < arg0.size(); i++) {
+							ParseObject row=arg0.get(i);
+							if(row.getString("type").equals("Schedule")){
+								addMarkerSchedule(row.getString("name"), row.getDouble("latitude"), row.getDouble("longitude"), row.getString("type"));
+							}
+							if(row.getString("type").equals("To Do")){
+								addMarkerTodo(row.getString("name"), row.getDouble("latitude"), row.getDouble("longitude"), row.getString("type"));
+							}
+							if(row.getString("type").equals("Meet")){
+								addMarkerMeet(row.getString("name"), row.getDouble("latitude"), row.getDouble("longitude"), row.getString("type"));
+							}
+						}
+						
+					}
+				}
+			}
+		});
+		
+	}
+
 }
 
